8440ca12d7ac34844a47b0e55503de486bfde9a5
Arthur <arlrcode@gmail.com>
2020-07-05 08:39:23 +0200

    creat pcol_easy function prototype

--- Colors.c
+++ Colors.c
@@ -12,5 +12,32 @@
 /* Defines */
 
 /* Functions prototype */
-
+/*
+ * @brief pcol_easy print color easy
+ * @detail print a string with different colors and decoration
+ * @param {ac_data_to_print} string to print
+ * @param {c_text_color} char to choose the text color 
+ * K = BLACK
+ * R = RED
+ * G = GREEN
+ * Y = YELLOW
+ * B = BLUE
+ * M = MAGENTA
+ * C = CYAN
+ * W = WHITE
+ * @param {c_back_ground_color} change the text background
+ * K = BLACK
+ * R = RED
+ * G = GREEN
+ * Y = YELLOW
+ * B = BLUE
+ * M = MAGENTA
+ * C = CYAN
+ * W = WHITE
+ * @param {c_decoration} text decoration
+ * B = BOLD
+ * U = UNDERLINE
+ * D = NO DECORATION
+ */
+int pcol_easy(char * ac_data_to_print, char c_text_color, char c_back_ground_color, char c_decoration)
 /* Functions body */
